7643f8bb0aaec50f89fd16bf0f53348c9909fcfd
Jeremy Elbourn <jelbourn@google.com>
2017-10-09 11:52:28 -0400

    Blank state

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <p>
+      app Works!
+    </p>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'app';
+}
